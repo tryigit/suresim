673346c610e966783529470d1aa2bd2c210a3fb1
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-11-20 20:39:58 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,40 +1,42 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id 'com.android.application'
 }
 
 android {
-    namespace = "com.thatmarcel.apps.suresim"
-    compileSdk = 34
+    namespace 'es.chiteroman.bootloaderspoofer'
+    compileSdk 35
+    buildToolsVersion '35.0.0'
+
+    packagingOptions {
+        resources {
+            excludes += "**"
+        }
+    }
 
     defaultConfig {
-        applicationId = "com.thatmarcel.apps.suresim"
-        minSdk = 31
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        applicationId "es.chiteroman.bootloaderspoofer"
+        minSdk 26
+        targetSdk 35
+        versionCode 40
+        versionName '4.0'
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            minifyEnabled true
+            shrinkResources true
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            signingConfig signingConfigs.debug
         }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
+        sourceCompatibility JavaVersion.VERSION_17
+        targetCompatibility JavaVersion.VERSION_17
     }
 }
 
 dependencies {
-    compileOnly(files("libs/api-100.aar"))
+    compileOnly 'de.robv.android.xposed:api:82'
+    implementation 'org.bouncycastle:bcpkix-jdk18on:1.78.1'
 }
